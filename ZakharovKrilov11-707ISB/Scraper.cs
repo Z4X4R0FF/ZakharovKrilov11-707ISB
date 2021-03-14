60427c773bca420c41f833924b487319d4c0b454
Крылов Антон <a.krylov@bars-open.ru>
2021-03-14 14:28:42 +0300

    добавлен tokenizer + рефакторинг

--- ZakharovKrilov11-707ISB/Scraper.cs
+++ ZakharovKrilov11-707ISB/Scraper.cs
@@ -1,54 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using HtmlAgilityPack;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Net;
-using System.Text;
 using System.IO;
 using System.Linq;
 
 namespace ZakharovKrilov11_707ISB
 {
-    public static class Scraper
+    public class Scraper
     {
-        public static Task<string> CallUrl(string fullUrl)
+        public Task<string> CallUrl(string fullUrl)
         {
-            var client = new HttpClient();
+            var httpClient = new HttpClient();
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls13;
-            client.DefaultRequestHeaders.Accept.Clear();
-            return client.GetStringAsync(fullUrl);
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            
+            return httpClient.GetStringAsync(fullUrl);
         }
 
-        public static IEnumerable<string> GetLinksFromHtml(HtmlDocument htmlDoc)
+        public IEnumerable<string> GetLinks(string html)
         {
+            var htmlDoc = ParseHtml(html);
             var programmerLinks = htmlDoc.DocumentNode.Descendants("li")
-                .Where(node => !node.GetAttributeValue("class", "").Contains("tocsection")).ToList();
+                .Where(node => !node.GetAttributeValue("class", "").Contains("tocsection"))
+                .ToList();
 
-            return programmerLinks.Where(r => r.FirstChild.Attributes.Count > 0)
-                .Select(r => "https://ru.wikipedia.org/" + r.FirstChild.Attributes[0].Value).ToList();
+            return programmerLinks
+                .Where(r => r.FirstChild.Attributes.Count > 0)
+                .Select(r => "https://ru.wikipedia.org/" + r.FirstChild.Attributes[0].Value)
+                .ToList();
         }
 
-        public static HtmlDocument ParseHtml(string html)
+        public HtmlDocument ParseHtml(string html)
         {
             var htmlDoc = new HtmlDocument();
             htmlDoc.LoadHtml(html);
             return htmlDoc;
         }
 
-        public static void WriteLinksToTxt(List<string> links)
+        public void WriteLinksToTxt(List<string> links)
         {
-            using (var writer = new StreamWriter("Htmls/index.txt"))
+            using var writer = new StreamWriter("Htmls/index.txt");
+            for (var i = 0; i < links.Count; i++)
             {
-                for (var i = 0; i < links.Count; i++)
-                {
-                    writer.WriteLine(i + " " + links[i]);
-                }
+                writer.WriteLine(i + " " + links[i]);
             }
         }
 
-        public static void WriteHtmlsToTxt(List<HtmlDocument> htmlDocuments)
+        public void WriteHtmlsToTxt(List<HtmlDocument> htmlDocuments)
         {
             for (var i = 0; i < htmlDocuments.Count; i++)
             {
